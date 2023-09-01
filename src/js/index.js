61204c4e5099750a807010f6cba8aa3116fd1e86
EduSG <eduardo.gomes.072020@gmail.com>
2023-09-01 13:36:24 -0300

    responsivo

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,21 @@
 const botoes_header = Array.from(document.querySelectorAll('.btn_page'));
-botoes_header.forEach( el => {
+botoes_header.forEach(el => {
     el.addEventListener('click', e => {
         e.preventDefault();
         el.classList.add('sublinhado')
     })
 })
+
+
+
+window.addEventListener('load', e => {
+    const altura = window.innerHeight;
+    const largura = window.innerWidth;
+    const wrapper_conhecimentos = document.querySelector('.wrapper_conhecimentos');
+    const wrapperp_conteudo = document.querySelector('.wrapperp_conteudo');
+
+    wrapper_conhecimentos.style.height = `70vh`;
+    wrapper_conhecimentos.style.minHeight = `${(altura / 100) * 70}px`;
+    wrapperp_conteudo.style.height = `62vh`;
+    wrapperp_conteudo.style.minHeight = `${(altura / 100) * 62}px`;
+});
